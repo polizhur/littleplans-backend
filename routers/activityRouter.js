5c31010c748956f9e49f129a5f99c0430c242e7a
polizhur <polizhur@gmail.com>
2021-11-02 11:24:09 +0100

    Add address to the endpoint

--- routers/activityRouter.js
+++ routers/activityRouter.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const Activity = require("../models").activity;
+const Address = require("../models").address;
 
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
-  const activities = await Activity.findAll();
+  const activities = await Activity.findAll({
+    include: [Address],
+  });
   res.json(activities);
 });
 
