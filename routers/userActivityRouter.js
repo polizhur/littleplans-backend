731d05c19a01d4f48f7de6b7ae2e15c73bfd13c9
polizhur <polizhur@gmail.com>
2021-11-02 21:53:55 +0100

    Set up a POST endpoint to add an activity to user's calendar

--- routers/userActivityRouter.js
+++ routers/userActivityRouter.js
@@ -22,4 +22,41 @@ router.delete("/:id", authMiddleware, async (req, res, next) => {
   }
 });
 
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const userActivityId = parseInt(req.params.id);
+    const specificUserActivity = await UserActivity.findByPk(userActivityId);
+    const loggedInUserId = req.user.id;
+    const activityUserId = specificUserActivity.userId;
+    if (loggedInUserId != activityUserId) {
+      return res
+        .status(400)
+        .send({ message: "Activity does not belong to this user" });
+    }
+    specificUserActivity.destroy();
+    res.status(200).send({ message: "Activity deleted" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { activityId } = req.body;
+    if (!activityId) {
+      return res
+        .status(400)
+        .send({ message: "An activityId must be provided" });
+    }
+    const userActivity = await UserActivity.create({
+      activityId,
+      userId,
+    });
+    res.status(200).send({ message: "Activity added", userActivity });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
