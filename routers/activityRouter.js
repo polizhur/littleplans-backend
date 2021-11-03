d49a8765b418111c99a4f748b9cabffb5bce6ee2
polizhur <polizhur@gmail.com>
2021-11-03 22:47:23 +0100

    Set up a new -POST endpoint to create a new activity

--- routers/activityRouter.js
+++ routers/activityRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const auth = require("../auth/middleware");
 const Activity = require("../models").activity;
 const Address = require("../models").address;
 const Category = require("../models").category;
@@ -26,6 +27,153 @@ router.get("/:id", async (req, res, next) => {
     }
   } catch (e) {
     next(e);
+  }
+});
+
+router.post("/", auth, async (req, res, next) => {
+  try {
+    const {
+      title,
+      imageUrl,
+      categoryId,
+      street,
+      number,
+      postcode,
+      city,
+      country,
+      longitude,
+      latitude,
+      date,
+      duration,
+      capacity,
+      description,
+      ageGroupId,
+      isParentRequired,
+    } = req.body;
+
+    if (!title) {
+      return res.status(400).send({ message: "An activity must have a title" });
+    }
+
+    if (!imageUrl) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have an image" });
+    }
+
+    if (!categoryId) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have an catigory" });
+    }
+
+    if (!street) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have an address" });
+    }
+
+    if (!number) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a house number" });
+    }
+
+    if (!postcode) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a postcode" });
+    }
+
+    if (!city) {
+      return res.status(400).send({ message: "An activity must have a city" });
+    }
+
+    if (!country) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a country" });
+    }
+
+    if (!longitude) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a longitude" });
+    }
+
+    if (!latitude) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a latitude" });
+    }
+
+    if (!description) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a description" });
+    }
+
+    if (!date) {
+      return res.status(400).send({ message: "An activity must have a date" });
+    }
+
+    if (!duration) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have duration" });
+    }
+
+    if (!capacity) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have a capacity" });
+    }
+
+    if (!ageGroupId) {
+      return res
+        .status(400)
+        .send({ message: "An activity must have an age group" });
+    }
+
+    const newActivity = await Activity.create({
+      title,
+      imageUrl,
+      categoryId,
+      description,
+      date,
+      duration,
+      capacity,
+      ageGroupId,
+      isParentRequired,
+      userId: req.user.id,
+    });
+
+    const newAddress = await Address.create({
+      street,
+      number,
+      postcode,
+      city,
+      country,
+      longitude,
+      latitude,
+      activityId: newActivity.id,
+    });
+
+    const ageGroup = await AgeGroup.findByPk(ageGroupId);
+
+    const category = await Category.findByPk(categoryId);
+
+    return res.status(201).send({
+      message: "Activity created",
+      activity: {
+        ...newActivity,
+        address: newAddress,
+        ageGroup: ageGroup,
+        category: category,
+      },
+    });
+  } catch (error) {
+    next(error);
   }
 });
 
